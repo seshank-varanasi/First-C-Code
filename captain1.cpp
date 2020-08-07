54a38ca34fa038a4a87fee4f0cec8e2c90a05623
seshank-varanasi <69347969+seshank-varanasi@users.noreply.github.com>
2020-08-07 16:09:31 +0530

    Add files via upload

---
+++ captain1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a;
+	char b;
+	float c;
+	double d;
+	cout<<"Size of int is "<<sizeof(a);
+	cout<<"\nSize of char is "<<sizeof(b);
+	cout<<"\nSize of float is "<<sizeof(c);
+	cout<<"\nSize of double is "<<sizeof(d);
+}
---
+++ captain2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	float a,b;
+	cout<<"Enter two numbers";
+	cin>>a>>b;
+	cout<<"\n Product of the two numbers is"<<a*b;
+}
